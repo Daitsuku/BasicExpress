dc1adcf376a84c762d2cec051e811336959b56c3
Toshisawa Daitsuku <65522118+Daitsuku@users.noreply.github.com>
2023-03-29 14:36:03 +0700

    first commit
    add file

---
+++ index.js
@@ -0,0 +1,29 @@
+// Import Express
+const express = require('express')
+
+// Import Router
+const urlRouter = require('./routes/url')
+
+// Create express object
+const app = express()
+
+// // กำหนด Folder สำหรับบอกตัว express ว่าไฟล์ css, images อยู่ path ไหน
+app.use(express.static('assets'))
+
+// เรียกใช้งาน Router
+app.use('', urlRouter)
+
+app.get('/api/restuarants', (req, res) => {
+    res.send('GET API')
+})
+app.post('/api/restuarants', (req, res) => {
+    res.send('POST API')
+})
+app.put('/api/restuarants/:id', (req, res) => {
+    res.send('PUT API')
+})
+
+// Run express server
+app.listen(3000, ()=> {
+    console.log('Listening to port 3000')
+})
